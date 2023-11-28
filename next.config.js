0c261b80d5c1e11494543ccd5998515dc115ff39
aleksandrsamraev <alex.shamrayev@gmail.com>
2023-11-28 18:23:37 +0300

    fix bug

--- next.config.js
+++ next.config.js
@@ -6,4 +6,28 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = {
+  ...nextConfig,
+  async headers() {
+    return [
+      {
+        // Включить CORS для всех маршрутов вашего приложения
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'OPTIONS, POST, GET, DELETE',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type',
+          },
+        ],
+      },
+    ];
+  },
+};
